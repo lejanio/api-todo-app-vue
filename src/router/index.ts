d91f27a927bbbfa235d35dcd535f4aaf1a473652
lejanio <janis.leonciks@gmail.com>
2022-02-23 09:17:40 +0200

    index.ts update

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,9 +10,6 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/item/:id",
     name: "item",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/ItemView.vue"),
   },
